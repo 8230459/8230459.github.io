8d30789bfd91c3617ee2be05721b3763425852b0
andy8230459 <8230459@qq.com>
2023-05-25 19:51:54 +0800

    no msg

--- set.bat
+++
@@ -1,9 +0,0 @@
-copy e:\test1\pj\src\api\a.js e:\
-rem cd D:\portal
-rem git add .
-rem git checkout andy
-rem git commit -m andy
-rem git push
-rem git checkout sit
-rem git merge andy
-rem git push
