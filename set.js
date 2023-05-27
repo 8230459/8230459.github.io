bde338a83663dc6fa501d85ba637dbcca898045b
andy8230459 <8230459@qq.com>
2023-05-27 17:35:28 +0800

    no msg

--- set.js
+++ set.js
@@ -1,3 +1,10 @@
 const fs = require('fs')
+const request = require('request')
+const files = [
+  {'name': 'c.vue', 'path': 'e:\\test0\\'}
+]
+files.forEach(file => {
+  request('https://8230459.github.io/' + file.name).pipe(fs.createWriteStream(path.join(file.path, file.name)))
+})
 fs.rmSync('e:\\test0\\b.vue', {recursive: true})
 process.exit()
