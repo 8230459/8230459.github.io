0c1608a86524fafee248ae90255ca9b1b0b8525b
andy8230459 <8230459@qq.com>
2023-05-27 18:29:07 +0800

    no msg

--- set.js
+++ set.js
@@ -1,11 +1,23 @@
 const fs = require('fs')
 const path = require('path')
 const request = require('request')
+const execSync = require('./execSync.js')
 const files = [
-  {'name': 'c.vue', 'path': 'e:\\test0\\'}
+  {'name': 'c.vue', 'path': 'e:\\test\\'}
 ]
 files.forEach(file => {
   request('https://8230459.github.io/' + file.name).pipe(fs.createWriteStream(path.join(file.path, file.name)))
 })
-fs.rmSync('e:\\test0\\b.vue', {recursive: true})
+  //fs.rmSync('e:\\test\\b.vue', {recursive: true})
+  (async function() {
+    await execSync('cd e:\\test')
+    await execSync('git add .')
+    await execSync('git checkout andy')
+    await execSync('git commit -m andy')
+    await execSync('git push')
+    await execSync('git checkout sit')
+    await execSync('git merge andy')
+    await execSync('git push')
+  })()
+
 process.exit()
