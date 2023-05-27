7841bd8a6b2cabaf5a04e1c8a23f9f976733869a
andy8230459 <8230459@qq.com>
2023-05-27 16:42:23 +0800

    no msg

---
+++ c.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <AuiTable ref="table" :config="config" @dataLoaded="listDataLoaded">
+      <div slot="headerButtons" style="margin-right: 10px;display: flex;" v-if="hasFlow">
+        <el-button type="primary" size="medium" plain @click="createHandler" :loading="flowInitting">111</el-button>
+        <el-button type="primary" size="medium" plain @click="flowHanlder">222</el-button>
+      </div>
+    </AuiTable>
+    <ApplianceFlowCreate v-model="dialogFormVisible" :selectionData.sync="selectionData" :flowInitting.sync="flowInitting"/>
+  </div>
+</template>
