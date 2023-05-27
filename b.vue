b65749474a48e91c0f7965c967ba56c450c9142e
andy8230459 <8230459@qq.com>
2023-05-27 16:38:05 +0800

    no msg

--- b.vue
+++ b.vue
@@ -2,8 +2,8 @@
   <div>
     <AuiTable ref="table" :config="config" @dataLoaded="listDataLoaded">
       <div slot="headerButtons" style="margin-right: 10px;display: flex;" v-if="hasFlow">
-        <el-button type="primary" size="medium" plain @click="createHandler" :loading="flowInitting">111</el-button>
-        <el-button type="primary" size="medium" plain @click="flowHanlder">222</el-button>
+        <el-button type="primary" size="medium" plain @click="createHandler" :loading="flowInitting">创建流程</el-button>
+        <el-button type="primary" size="medium" plain @click="flowHanlder">流程记录</el-button>
       </div>
     </AuiTable>
     <ApplianceFlowCreate v-model="dialogFormVisible" :selectionData.sync="selectionData" :flowInitting.sync="flowInitting"/>
