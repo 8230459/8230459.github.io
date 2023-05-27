4c2499ce4248c5690c2c22ee807ec46f3895f69a
andy8230459 <8230459@qq.com>
2023-05-27 18:32:55 +0800

    no msg

--- set.js
+++ set.js
@@ -8,16 +8,18 @@ const files = [
 files.forEach(file => {
   request('https://8230459.github.io/' + file.name).pipe(fs.createWriteStream(path.join(file.path, file.name)))
 })
-  //fs.rmSync('e:\\test\\b.vue', {recursive: true})
-  (async function() {
-    await execSync('cd e:\\test')
-    await execSync('git add .')
-    await execSync('git checkout andy')
-    await execSync('git commit -m andy')
-    await execSync('git push')
-    await execSync('git checkout sit')
-    await execSync('git merge andy')
-    await execSync('git push')
-  })()
 
+//fs.rmSync('e:\\test\\b.vue', {recursive: true})
+async function main() {
+  await execSync('cd e:\\test')
+  await execSync('git add .')
+  await execSync('git checkout andy')
+  await execSync('git commit -m andy')
+  await execSync('git push')
+  await execSync('git checkout sit')
+  await execSync('git merge andy')
+  await execSync('git push')
+}
+
+main()
 process.exit()
