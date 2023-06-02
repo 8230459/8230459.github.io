236fc00682e2eea89bfb1a8858eeeedb9ca764ff
andy8230459 <8230459@qq.com>
2023-06-02 15:22:01 +0800

    no msg

--- build.js
+++ build.js
@@ -1,18 +1,27 @@
 const fs = require('fs')
 const path = require('path')
 const secret = require('./secret')
-let count = 0
-let total = 0
-fs.readdir('s', (err, files) => {
+fs.rm('./b', {recursive: true}, err => {
   if (err) return
-  total = files.length
-  for (let file of files) {
-    fs.readFile('./s/' + file, (err, data) => {
+  console.log(1)
+  fs.mkdir('./b', err => {
+    if (err) return
+    let count = 0
+    let total = 0
+    fs.readdir('s', (err, files) => {
       if (err) return
-      fs.writeFile(path.join('./b/', file), secret.Lock(data.toString()), err => {
-        count++
-        if (total === count) require('./execSync')('publish.bat')
-      })
+      total = files.length
+      for (let file of files) {
+        fs.readFile(path.join('./s/', file), (err, data) => {
+          if (err) return
+          fs.writeFile(path.join('./b/', file), secret.Lock(data.toString()), err => {
+            count++
+            if (total === count) require('./execSync')('publish.bat')
+          })
+        })
+      }
     })
-  }
+  })
 })
+
+
