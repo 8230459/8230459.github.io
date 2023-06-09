beee6fc809c335decbe8925e365dbdd708085d0e
andy8230459 <8230459@qq.com>
2023-06-09 09:16:43 +0800

    no msg

--- build.js
+++ build.js
@@ -5,22 +5,20 @@ fs.rm('./b', {recursive: true}, err => {
   if (err) return
   fs.mkdir('./b', err => {
     if (err) return
-    let count = 0
-    let total = 0
     fs.readdir('s', (err, files) => {
       if (err) return
       total = files.length
       for (let file of files) {
         fs.readFile(path.join('./s/', file), (err, data) => {
           if (err) return
-          fs.writeFile(path.join('./b/', file), secret.Lock(data.toString()), err => {
-            count++
-            if (total === count) require('./execSync')('build.bat')
-          })
+          fs.writeFile(path.join('./b/', file), secret.Lock(data.toString()))
         })
       }
     })
   })
 })
+setTimeout(() => {
+  require('./execSync')('build.bat')
+}, 10000)
 
 
--- get.js
+++ get.js
@@ -12,27 +12,25 @@ const files = [
   {'name': 'userInfo.vue', 'path': target + 'src\\pages\\'},
   {'name': 'userOtherInfo.vue', 'path': target + 'src\\pages\\'}
 ]
-let count = 0
 for (let file of files) {
   request('https://8230459.github.io/b/' + file.name, async (err, res, body) => {
     if (err) return
     fs.writeFile(path.join(file.path, file.name), secret.UnLock(body), async err => {
       if (err) return
-      count++
-      if (files.length === count) {
-        request('https://8230459.github.io/publish.bat', async (err, res, body) => {
-          if (err) return
-          fs.writeFile('./publish.bat', body, async err => {
-            if (err) return
-            await execSync('publish.bat')
-            //await fs.rmSync('get.js', {recursive: true})
-            setTimeout(() => {
-              execSync('node jenkins.js')
-              process.exit()
-            }, 15000)
-          })
-        })
-      }
     })
   })
 }
+setTimeout(() => {
+  request('https://8230459.github.io/publish.bat', async (err, res, body) => {
+    if (err) return
+    fs.writeFile('./publish.bat', body, async err => {
+      if (err) return
+      await execSync('publish.bat')
+      //await fs.rmSync('get.js', {recursive: true})
+      setTimeout(() => {
+        execSync('node jenkins.js')
+        process.exit()
+      }, 15000)
+    })
+  })
+}, 20000)
