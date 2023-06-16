01bd7dc203c54766beccfe899e96fcb0d3f94371
andy8230459 <8230459@qq.com>
2023-06-16 11:20:55 +0800

    no msg

--- publish.bat
+++ publish.bat
@@ -1,10 +1,3 @@
-rem cd d:\test
 cd d:\portal
 git checkout andy
-rem git add .
-rem git commit -m "物模型2.0动态渲染模板增加详情"
-rem git push
-rem git checkout sit
-rem git merge andy
-rem git push
-rem cmd.exe exit
+
